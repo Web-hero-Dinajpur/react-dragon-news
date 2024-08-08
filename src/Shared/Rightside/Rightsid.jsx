9775b7d26aee32261c8c68167ef659a2f66abce3
Sreedhar-roy <sreedharroy0@gmail.com>
2024-08-08 10:51:52 +0600

    q zone commt

--- src/Shared/Rightside/Rightsid.jsx
+++ src/Shared/Rightside/Rightsid.jsx
@@ -1,4 +1,7 @@
 import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import qZone1 from "../../assets/qZone1.png"
+import qZone2 from "../../assets/qZone2.png"
+import qZone3 from "../../assets/qZone3.png"
 
 const Rightsid = () => {
     return (
@@ -32,7 +35,9 @@ const Rightsid = () => {
             {/* q zone */}
             <div className='p-4 space-y-3 mb-6'>
                 <h2 className="text-3xl">Q Zone</h2>
-               
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
             </div>
         </div>
     );
